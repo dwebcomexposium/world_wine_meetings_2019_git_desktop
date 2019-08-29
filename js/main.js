cf403b2115cb557678b62246f2d42f6d98106246
omicronn <thomas.bastide@omicronn.fr>
2019-08-29 13:40:18 +0200

    modif js pour chiffres

--- js/main.js
+++ js/main.js
@@ -3,34 +3,35 @@ $(function() {
 	$(window).scroll(testScroll);
 	var viewed = false;
 
+	function isScrolledIntoView(elem) {
+	    var docViewTop = $(window).scrollTop();
+	    var docViewBottom = docViewTop + $(window).height();
+
+	    var elemTop = $(elem).offset().top;
+	    var elemBottom = elemTop + $(elem).height();
+
+	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
+	function testScroll() {
+	  if (isScrolledIntoView($("#chiffres")) && !viewed) {
+	      viewed = true;
+	      $('.compteur').each(function () {
+	      $(this).prop('Counter',0).animate({
+	          Counter: $(this).text()
+	      }, {
+	          duration: 3000,
+	          easing: 'swing',
+	          step: function (now) {
+	              $(this).text(Math.ceil(now));
+	          }
+	      });
+	    });
+	  }
+	}
 });
 
-function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
 
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-function testScroll() {
-  if (isScrolledIntoView($("#chiffres")) && !viewed) {
-      viewed = true;
-      $('.compteur').each(function () {
-      $(this).prop('Counter',0).animate({
-          Counter: $(this).text()
-      }, {
-          duration: 3000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now));
-          }
-      });
-    });
-  }
-}
 
 /* FIN CHIFFRES */
 
