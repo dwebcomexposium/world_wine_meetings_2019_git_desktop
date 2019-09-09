015195d5f562100b69232bf070669e0d23d0010c
omicronn <thomas.bastide@omicronn.fr>
2019-09-09 18:26:18 +0200

    modif js menu mobile

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ $(function() {
 	$( out ).appendTo( "body" );
 	
 	/* menu mobile */
-	var menu = '<div id="mobile_menu"></div>'
+	var menu = '<div id="mobile_menu"><div id="mobile_menu_close"><div class="mobile_menu_close" id="mobile_menu_close_1"></div><div class="mobile_menu_close" id="mobile_menu_close_2"></div></div></div>';
 	$( menu ).appendTo( "body" );	
 	$( ".site-banner .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
 	$( ".site-banner .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
