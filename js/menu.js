961b9716c9b5547662bdbf83c7aa55e1007156d5
omicronn <thomas.bastide@omicronn.fr>
2019-09-09 18:53:15 +0200

    correction surcharges css

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,7 @@ $(function() {
 	var out = '<div id="burger"><div></div><div></div><div></div></div>';
 	$( out ).appendTo( "body" );
 	
+	
 	/* menu mobile */
 	var menu = '<div id="mobile_menu"><div id="mobile_menu_close"><div class="mobile_menu_close" id="mobile_menu_close_1"></div><div class="mobile_menu_close" id="mobile_menu_close_2"></div></div></div>';
 	$( menu ).appendTo( "body" );	
@@ -11,5 +12,13 @@ $(function() {
 	$( ".site-footer .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
 	$( ".global-search-form .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
 	
+	
+	/* action */
+	$("#burger").click(function() {
+		  $( "#mobile_menu" ).fadeIn();
+	});
+	$("#mobile_menu_close").click(function() {
+		  $( "#mobile_menu" ).fadeOut();
+	});
 	
 });
