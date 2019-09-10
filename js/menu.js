ee183bbae6263f92fd9c089d3fcf0f546480c771
omicronn <thomas.bastide@omicronn.fr>
2019-09-10 09:19:45 +0200

    modif js menu mobile

--- js/menu.js
+++ js/menu.js
@@ -8,12 +8,13 @@ $(function() {
 	/* menu mobile */
 	var menu = '<div id="mobile_menu"><div id="mobile_menu_close"><div class="mobile_menu_close" id="mobile_menu_close_1"></div><div class="mobile_menu_close" id="mobile_menu_close_2"></div></div></div>';
 	$( menu ).appendTo( "body" );	
-	$( ".site-banner .quicklinks .ql-list:nth-child(1)" ).clone().appendTo( "#mobile_menu" );
-	$('<div class="mobile_menu_sep"></div>').appendTo( "#mobile_menu" );
+	$( ".site-banner .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
+	
 	$( ".site-footer .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
-	$('<div class="mobile_menu_sep"></div>').appendTo( "#mobile_menu" );
+	
 	$( ".global-search-form .quicklinks .ql-list" ).clone().appendTo( "#mobile_menu" );
 	
+	$( ".site-banner .quicklinks:nth-child(1)" ).after( '<div class="mobile_menu_sep"></div>' );
 	
 	/* action */
 	$("#burger").click(function() {
